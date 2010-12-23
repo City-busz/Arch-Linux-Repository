7e50e913f6c0a9a8075f90577f80a6b828d12db6
György Balló <ballogy@freestart.hu>
2010-12-23 05:20:06 +0100

    traveling-salesman: removed, because it's not work very well

--- maps/traveling-salesman/traveling-salesman.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-CP=/usr/share/java/traveling-salesman/traveling-salesman-1.0.2.jar
-
-exec $JAVA_HOME/bin/java -cp $CP -jar /usr/share/java/traveling-salesman/traveling-salesman-1.0.2.jar
